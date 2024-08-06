c433fbab2ff515779b01b01bbe3344668756cd01
AejiCheon <144427408+aejicheon@users.noreply.github.com>
2024-08-07 00:28:51 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a-b);
+	
+	}
+
+}

8e307f935b718dd4d6f41f9c249935cee1331f37
AejiCheon <144427408+aejicheon@users.noreply.github.com>
2024-08-07 00:25:34 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+    int a,b;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    if(a>0) {
+        if(b>0) {
+            printf("1");
+        }
+        else {
+            printf("4");
+        }
+    }
+    else if(a<0) {
+        if(b>0) {
+            printf("2");
+        }
+        else {
+            printf("3");
+        }
+    }
+}

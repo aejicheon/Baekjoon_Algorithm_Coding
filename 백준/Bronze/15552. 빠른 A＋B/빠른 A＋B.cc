2315eabe9e22bc0148a8603d4c1a99c18563f055
AejiCheon <144427408+aejicheon@users.noreply.github.com>
2024-08-14 18:42:34 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 556 ms, Memory: 5800 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int c, a, b, sum=0;
+
+    cin >> c;
+    int s[c];
+
+    for(int i=0; i<c; i++) {
+        a=0; b=0;
+        cin >> a >> b;
+        s[i] = a+b;
+    }
+    for(int i=0; i<c; i++) {
+        cout << s[i] << "\n";
+    }
+}

1da9b4ce808782541e5fe4b8c235fe97f0fa5336
AejiCheon <144427408+aejicheon@users.noreply.github.com>
2024-11-25 19:13:46 +0900

    [Bronze II] Title: 보너스 점수, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/17389. 보너스 점수/보너스 점수.java
+++ 백준/Bronze/17389. 보너스 점수/보너스 점수.java
@@ -1,28 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        String text = st.nextToken();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner s = new Scanner(System.in);
-		int length,bp = 0,cnt = 0;
-		String ss;
-		
-		length = s.nextInt();
-		ss = s.next();
-		
-		for(int i = 0; i < ss.length(); i++) {
-			if(ss.charAt(i) == 'O') {
-				cnt += ((i+1)+bp);
-				bp += 1;
-			}
-			else {
-				bp = 0;
-			}
-		}
-		
-		System.out.println(cnt);
-		
-	}
+        int sum = 0;
+        int bonus = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == 79) {
+                sum += (i + 1) + bonus;
+                bonus++;
+            } else if (text.charAt(i) == 88) {
+                bonus = 0;
+            }
+        }
 
+
+
+        bw.write(sum + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
